785b98ca1a58c1d06da61fe371bec39aa0fa956d
pc <pc@DESKTOP-B6E9JH5>
2019-08-08 16:38:03 +0800

    修改自己的coding

--- src/cn/changcheng/HelloWorld.java
+++ src/cn/changcheng/HelloWorld.java
@@ -3,6 +3,6 @@ package cn.changcheng;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("helloworld-------");
+		System.out.println("helloworld-------修改自己的coding");
 	}
 }
